2fae7e919d8b5d6143dc484ccbcc8c5050570a9b
GCausse <GCausse@epri.com>
2023-08-01 15:38:17 -0400

    Minor style change

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.header('Tapasvi Lolla Proudly Presents:')
 st.header('The P229 internal toolbox for the Materials Team')
 st.write('(With some help from Gerard Causse)')
 
-st.write('Welcome to the internal EPRI toolbox!')
+st.subheader('Welcome to the internal EPRI toolbox!')
 
 st.write('Please direct all complaints to tlolla@epri.com')
 st.write('Please direct all compliments to gcausse@epri.com')
